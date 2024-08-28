2fed87edc3b74d7fa0827edff341d617050efef3
Zohaib Ahmad <zohaibsattar1039@gmail.com>
2024-08-28 11:43:58 +0500

    Signup page added

---
+++ app/register/page.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+const Register = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-lg border border-gray-300 w-full max-w-md">
+        <h2 className="text-2xl font-semibold text-gray-700 text-center mb-6">
+          Sign Up
+        </h2>
+        <form>
+          <div className="mb-4">
+            <Label
+              htmlFor="name"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Name
+            </Label>
+            <Input
+              type="text"
+              id="name"
+              className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-200"
+              placeholder="Enter your name"
+            />
+          </div>
+          <div className="mb-4">
+            <Label
+              htmlFor="email"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Email
+            </Label>
+            <Input
+              type="email"
+              id="email"
+              className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-200"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="mb-6">
+            <Label
+              htmlFor="password"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Password
+            </Label>
+            <Input
+              type="password"
+              id="password"
+              className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-200"
+              placeholder="Enter your password"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-black text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline hover:bg-gray-800"
+          >
+            Sign Up
+          </button>
+        </form>
+        <p className="text-center text-gray-600 text-sm mt-6">
+          Already have an account?{" "}
+          <Link
+            href="/login"
+            className="text-indigo-500 hover:text-indigo-700 font-semibold"
+          >
+            Go to Login
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
