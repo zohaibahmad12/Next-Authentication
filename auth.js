2d8c613d4f8ac1a1ff9f43a8f94f76c2b3578ad7
Zohaib Ahmad <zohaibsattar1039@gmail.com>
2024-09-01 01:29:02 +0500

    credential provider defined

--- auth.js
+++ auth.js
@@ -1,5 +1,12 @@
 import NextAuth from "next-auth";
-
+import Credentials from "next-auth/providers/credentials";
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+    }),
+  ],
 });
