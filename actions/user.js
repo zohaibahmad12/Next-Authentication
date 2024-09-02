26d82f7ee18aed5971e3817dce5eaedbf6eaa672
Zohaib Ahmad <zohaibsattar1039@gmail.com>
2024-09-02 22:44:38 +0500

    credentials based authentication implemented using auth builtin login page

--- actions/user.js
+++ actions/user.js
@@ -2,11 +2,12 @@
 import connectDB from "@/lib/db";
 import { User } from "@/models/User";
 import { redirect } from "next/navigation";
+import { hash } from "bcryptjs";
 export const signupUser = async (formData) => {
   const name = formData.get("name");
   const email = formData.get("email");
-  const password = formData.get("password");
-
+  let password = formData.get("password");
+  password = await hash(password, 10);
   await connectDB();
   await User.create({ name, email, password });
   redirect("/login");
--- auth.js
+++ auth.js
@@ -1,11 +1,28 @@
 import NextAuth from "next-auth";
+import connectDB from "./lib/db";
+import { User } from "./models/User";
 import Credentials from "next-auth/providers/credentials";
+import { compare } from "bcryptjs";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Credentials({
       credentials: {
         email: {},
         password: {},
+      },
+
+      authorize: async (credentials) => {
+        let user = null;
+        await connectDB();
+        user = await User.findOne({ email: credentials.email });
+        if (!user) {
+          throw new Error("User not found.");
+        }
+        let passwordMatch = await compare(credentials.password, user.password);
+        if (!passwordMatch) {
+          throw new Error("Invalid Password");
+        }
+        return user;
       },
     }),
   ],
